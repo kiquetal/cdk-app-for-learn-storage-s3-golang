025dc337d929a513bf932c3f25c17e5badfc3bdf
kiquetal <kiquetal@gmail.com>
2024-12-31 16:35:50 -0300

    Initial commit

---
+++ test/cdk-app-for-learn-storage-s3-golang.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as CdkAppForLearnStorageS3Golang from '../lib/cdk-app-for-learn-storage-s3-golang-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-app-for-learn-storage-s3-golang-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new CdkAppForLearnStorageS3Golang.CdkAppForLearnStorageS3GolangStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
