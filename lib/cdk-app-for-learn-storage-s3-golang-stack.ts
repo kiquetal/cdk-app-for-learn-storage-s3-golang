c5d9d223f06eabcd14fffeabd8f2d979f631cb23
kiquetal <kiquetal@gmail.com>
2025-01-01 13:24:18 -0300

    adding creation of user and group with admin access

--- lib/cdk-app-for-learn-storage-s3-golang-stack.ts
+++ lib/cdk-app-for-learn-storage-s3-golang-stack.ts
@@ -1,5 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
+import * as s3 from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
+import {CfnAccessKey, Group, Role, User} from "aws-cdk-lib/aws-iam";
+import {aws_iam, CfnOutput} from "aws-cdk-lib";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class CdkAppForLearnStorageS3GolangStack extends cdk.Stack {
@@ -12,5 +15,34 @@ export class CdkAppForLearnStorageS3GolangStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'CdkAppForLearnStorageS3GolangQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
+
+   const cliUser = new User(this, 'CdkAppForLearnStorageS3GolangUser', {
+     userName: 'golanguser'
+   })
+    const group = new Group(this, 'CdkAppForLearnStorageS3GolangGroup', {
+      groupName: "managers"
+    })
+
+
+
+
+      group.addManagedPolicy({
+            managedPolicyArn: 'arn:aws:iam::aws:policy/AdministratorAccess'
+      })
+
+
+    group.addUser(cliUser)
+    const accessKey = new CfnAccessKey(this, 'CdkAppForLearnStorageS3GolangAccessKey', {
+        userName: cliUser.userName
+    })
+
+    new CfnOutput(this, "accessKeyId", {
+        value: accessKey.ref
+    })
+
+    new CfnOutput(this,"secretAccessKey",{
+        value: accessKey.attrSecretAccessKey
+    })
+
   }
 }
